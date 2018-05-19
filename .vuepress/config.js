5182ff9dc3e97eb0d6b8e44bdf77918fb85692fa
Julien Thoma <julien.thoma89@gmail.com>
2018-05-19 21:05:02 +0200

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "Vuepress test",
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    nav: [
+      {
+        text: "Home",
+        link: "/"
+      },
+      {
+        text: "test",
+        link: "/test/"
+      }
+    ]
+  }
+};
---
+++ somefile.md
@@ -0,0 +1,11 @@
+# Some File without any link
+With no Link
+
+
+``` javascript
+var someBool = true;
+consle.log(a);
+
+const fu = (baz, troll) => !someBool;
+
+```
